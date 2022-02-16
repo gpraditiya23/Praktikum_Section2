3409b6d3025f92de62f64623c45dc0c9fdf6bf81
gilanprad <82693693+gpraditiya23@users.noreply.github.com>
2022-02-16 11:30:53 +0700

    main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Praktikum Section 2</h2>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Date and Time.</button>
+
+<p id="demo"></p>
+
+</body>
+</html> 
